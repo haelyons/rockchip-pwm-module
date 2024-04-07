75013ce8bc5af7b6d94877b22c661b408237ecb3
Helios Lyons <helioslyons@icloud.com>
2024-04-07 19:10:08 +0100

    Update SK6812 defines

--- direct_pwm_access_rk3568.c
+++ direct_pwm_access_rk3568.c
@@ -1,13 +1,3 @@
-/* WIP RESOURCES
-
-    PrimeCell DMA Controller (PL330) Technical Reference:
-    https://developer.arm.com/documentation/ddi0424/a/
-
-    Collated ARM SoC Device Assignment Notes
-    https://cwshu.github.io/arm_virt_notes/notes/dma/pl330.html
-    
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -15,46 +5,67 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-#define FPWM    1200    // Pulse period (ns) -- 1.2kHZ
-#define RST     50000   // Minimum RESET
-#define T0H     300     // high for 0 (ns)
-#define T0L     900     // low for 0 (ns)
-#define T1H     600     // high for 1 (ns) 
-#define T1L     600     // low for 1 (ns)
+// 
+/* -------- Key Resources --------
+- RK3568 TRM Part I V1.3
+- RK3568 TRM Part II V1.1 (Can't find 1.3)
+- Rockchip Linux source (pwm-rockchip.c)
+- Firefly ROC-RK3568-PC PWM examples (pwm-firefly.c)
+- Raspberry Pi 4 DMA Examples (specifically for SK6812 or ws281x LEDSTRIPS -> see rpi_ws281x library for full, quality RPi implementation)
 
-// If non-zero, print debug information
-#define DEBUG   0       // Print non-zero print debug
+TODO 
+- Add the RPi DMA one
+- Additional useful sources
+*/
 
-// Strip characteristics
-#define LEDS    57      // Number of LEDs
+// -------- Debug --------
+#define DEBUG   1       // Print non-zero print debug
 
-// -------- Register Definitions (Rockchip RK3568 TRM Part I V1.3) --------
-#define DMAC0_NS    0xFE530000 // DMA Base address (placeholder, use your actual)
-#define PWM2_BASE   0xFE6F0000 
+// -------- Register Definitions --------
+#define DMAC0_NS                0xFE530000 // DMA Base address (placeholder, use your actual)
+#define PWM2_BASE               0xFE6F0000 
+#define PWM2_PERIOD_OFFSET      0x0024 
+#define PWM2_DUTY_OFFSET        0x0028
+#define PWM2_CTRL_OFFSET        0x002C 
 
-// Offsets for PWM2 registers
-#define PWM2_PERIOD_OFFSET  0x0024 
-#define PWM2_DUTY_OFFSET    0x0028
-#define PWM2_CTRL_OFFSET    0x002C 
-#define PWM2_EN             (1 << 0)
+// -------- Register Configuration --------
+#define PWM_CTRL_TIMER_EN	    (1 << 0)
+#define PWM_CTRL_OUTPUT_EN	    (1 << 3)
 
-// Configure PWM settings
-#define PWM_PERIOD      10000 // Period in some clock units
-#define PWM_DUTY_CYCLE  5000  // Initial duty cycle
+#define PWM_ENABLE			    (1 << 0)
+#define PWM_CONTINUOUS		    (1 << 1)
+#define PWM_DUTY_POSITIVE	    (1 << 3)
+#define PWM_DUTY_NEGATIVE	    (0 << 3)
+#define PWM_INACTIVE_NEGATIVE	(0 << 4)
+#define PWM_INACTIVE_POSITIVE	(1 << 4)
+#define PWM_POLARITY_MASK	    (PWM_DUTY_POSITIVE | PWM_INACTIVE_POSITIVE)
+#define PWM_OUTPUT_LEFT		    (0 << 5)
+#define PWM_OUTPUT_CENTER	    (1 << 5)
+#define PWM_LOCK_EN		        (1 << 6)
+#define PWM_LP_DISABLE		    (0 << 8)
 
+// TODO delete, running in continuous mode
+#define PWM_ONESHOT_COUNT_SHIFT 24
+#define PWM_ONESHOT_COUNT_MASK	(0xff << PWM_ONESHOT_COUNT_SHIFT)
+#define PWM_ONESHOT_COUNT_MAX	256
+
+#define PWM_REG_INTSTS(n)	    ((3 - (n)) * 0x10 + 0x10)
+#define PWM_REG_INT_EN(n)	    ((3 - (n)) * 0x10 + 0x14)
+
+#define PWM_CH_INT(n)		    BIT(n)
+
+// -------- SK6812 Specification --------
+#define LEDS                    57           // Number of LEDs
+#define T0H                     300          // Duty cycle high / low for 0
+#define T0L                     900 
+#define T1H                     600          // Duty cycle high / low for 1
+#define T1L                     600
+#define FPWM                    (T0H + T0L)  // PWM frequency (period)
+#define RST                     50000        // min. reset value
+
+// -------- Memory Macros --------
 #define PAGE_SIZE         0x1000  // Size of memory page
 #define PAGE_ROUNDUP(n)   ((n)%PAGE_SIZE==0 ? (n) : ((n)+PAGE_SIZE)&~(PAGE_SIZE-1)) // Round up to nearest page
-
-#define T0H 300     
-#define T0L 900  
-#define T1H 600 
-#define T1L 600
-
-// -------- FUNCTION PROTOTYPES --------
-// TODO terminate function
-void configure_pwm(int period, int duty_cycle, void* pwm_regs);
-void start_pwm(void* pwm_regs);
-void stop_pwm(void* pwm_regs);
 
 // ----- MGMT -----
 void FAIL(const char *msg) 
@@ -100,7 +111,8 @@ void unmap_segment(void *mem, int size)
     munmap(mem, PAGE_ROUNDUP(size));
 }
 
-uint32_t set_pwm_duty_cycle_ns(void *pwm_regs, uint32_t duration_ns) {
+uint32_t set_pwm_duty_cycle_ns(void *pwm_regs, uint32_t duration_ns) 
+{
     uint32_t *duty_reg = (uint32_t *)pwm_regs + (PWM2_DUTY_OFFSET / sizeof(uint32_t));
     // TODO: what are PWM peripheral register units? ns?
     *duty_reg = duration_ns; 
@@ -108,137 +120,114 @@ uint32_t set_pwm_duty_cycle_ns(void *pwm_regs, uint32_t duration_ns) {
 }
 
 // ----- PROGRAM -----
-int main() {
-    int fd;
-    void *pwm_regs;
+int main() 
+{
+    fprintf(stdout, "[LIGHT] Using physical address 0x%x\n", PWM2_BASE);
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd < 0)
+    {
+        printf("[LIGHT] ERROR: Failed to open /dev/mem\n");
+    }
 
-    fd = open("/dev/mem", O_RDWR | O_SYNC);
-    pwm_regs = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PWM2_BASE);
+    void *pwm_regs = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PWM2_BASE);
     if (pwm_regs == NULL) 
     {
-        printf("Error mapping PWM registers!\n");
-        return 1;
-    } 
+        printf("[LIGHT] ERROR: Failed to mmap() PWM physical register address to virtual memory\n");
+        return -1;
+    }
     else 
     { 
-      printf("PWM2 register mapped at %p\n", pwm_regs); // Print mapped virtual mem addr
+        printf("[LIGHT] PWM register mapped at %p\n", pwm_regs); // Print mapped virtual mem addr
     }
-    close(fd);
+    close(fd); // can close file descriptor without invalidating the mapping (see mmap() man page)
     
-    uint8_t led_data = 0b10110100; // led test data
-    int i;
-
-    printf("Writing period value: %d\n", PWM_PERIOD);
-    printf("Writing duty cycle value: %d\n", PWM_DUTY_CYCLE);
+    fprintf(stdout, "TP0");
 
     // assume shortest possible period for continuous mode
     uint32_t duty_reg_val = set_pwm_duty_cycle_ns(pwm_regs, T0H);
-    printf("\n");
-    uint32_t *period_reg = (uint32_t *)pwm_regs + (PWM2_PERIOD_OFFSET / sizeof(uint32_t));
-    *period_reg = T0H + T0L; 
     
-    printf("Set PWM2 period (%d) with bus value %d\n", PWM_PERIOD, *period_reg);
-    printf("Set PWM2 duty cycle (%d) with bus value %u\n", PWM_DUTY_CYCLE, duty_reg_val);
+    uint32_t *period_reg = (uint32_t *)pwm_regs + (PWM2_PERIOD_OFFSET / sizeof(uint32_t));
+    *period_reg = T0H + T0L;
+
+    fprintf(stdout, "TP1");
+
+    printf("Set PWM2 period (%d) with bus value %d\n", T0H + T0L, *period_reg);
+    printf("Set PWM2 duty cycle (%d) with bus value %u\n", T0H, duty_reg_val);
 
     uint32_t *ctrl_reg = (uint32_t *)pwm_regs + (PWM2_CTRL_OFFSET / sizeof(uint32_t));
-    *ctrl_reg |= PWM2_EN;
+    *ctrl_reg |= PWM_ENABLE;
     uint32_t ctrl_reg_value = *ctrl_reg;
 
-    if (ctrl_reg_value & PWM2_EN) 
+    printf("TP2");
+
+    if (ctrl_reg_value & PWM_ENABLE) 
     {
-        printf("Enable PWM2 bit set correctly\n");
+        printf("[LIGHT] PWM2 Enable bus value correct\n");
     } 
     else 
     {
-        printf("Enable PWM2 bit not set\n");
+        printf("[LIGHT] PWM2 Enable bus value incorrect\n");
     }
-
-    // send SK6812 sample data
-    for (i = 0; i < 8; i++) 
-    {
-        if (led_data & (1 << (7 - i))) 
-        {
-            printf("1");
-            set_pwm_duty_cycle_ns(pwm_regs, T1H);
-            usleep(T1L / 1000); // Rough delay if no high-precision timer available
-        } 
-        else 
-        {
-            printf("0");
-            set_pwm_duty_cycle_ns(pwm_regs, T0H);
-            usleep(T0L / 1000); 
-        }
-    }
-    printf("\n");
-    return 0;
 }
-    /*
-    //signal(SIGINT, terminate);
-
-    // Map registers 
-    void *pwm2_regs = map_segment((void*)PWM2_BASE, PAGE_SIZE);
-    if (pwm2_regs == NULL) 
-    {
-        printf("Error mapping PWM registers!\n");
-        return 1;
-    } 
-    else 
-    { 
-      printf("PWM2 register mapped at %p\n", pwm2_regs); // Print mapped addr
-    }
-
-    // Configure and start PWM
-    pwm_configure(PWM_PERIOD, PWM_DUTY_CYCLE, pwm2_regs);
-    pwm_start(pwm2_regs);
-
-    if (pwm_enabled(pwm2_regs)) 
-    {
-      printf("PWM2 enabled!\n");
-    } 
-    else 
-    {
-      printf("PWM2 enabled failed...\n");
-    }
-
-    printf("PWM configured. Press Ctrl+C to exit.\n"); 
-
-    while(1) 
-    {
-        sleep(1); 
-    }
-
-    pwm_stop(pwm2_regs);
-    unmap_segment(pwm2_regs, PAGE_SIZE);
-    exit(0);
-    */
 
 // ----- PWM ----- 
 void pwm_configure(int period, int duty_cycle, void* pwm_regs) 
 {
-  printf("Writing period value: %d\n", period);
-  printf("Writing duty cycle value: %d\n", duty_cycle);
+    printf("Writing period value: %d\n", period);
+    printf("Writing duty cycle value: %d\n", duty_cycle);
 
-  uint32_t *pwm2_regs = (uint32_t*)pwm_regs;
-  pwm2_regs[PWM2_PERIOD_OFFSET / sizeof(uint32_t)] = period;
-  pwm2_regs[PWM2_DUTY_OFFSET / sizeof(uint32_t)] = duty_cycle;
+    uint32_t *pwm2_regs = (uint32_t*)pwm_regs;
+    pwm2_regs[PWM2_PERIOD_OFFSET / sizeof(uint32_t)] = period;
+    pwm2_regs[PWM2_DUTY_OFFSET / sizeof(uint32_t)] = duty_cycle;
 
-  // Calculate addresses
-  uint32_t *period_reg_addr = pwm2_regs + (PWM2_PERIOD_OFFSET / sizeof(uint32_t));
-  uint32_t *duty_reg_addr = pwm2_regs + (PWM2_DUTY_OFFSET / sizeof(uint32_t));
+    // Calculate addresses
+    uint32_t *period_reg_addr = pwm2_regs + (PWM2_PERIOD_OFFSET / sizeof(uint32_t));
+    uint32_t *duty_reg_addr = pwm2_regs + (PWM2_DUTY_OFFSET / sizeof(uint32_t));
 
-  printf("PWM2 period register at %p, value: %u\n", period_reg_addr, *period_reg_addr);
-  printf("PWM2 duty cycle register at %p, value: %u\n", duty_reg_addr, *duty_reg_addr);
+    printf("PWM2 period register at %p, value: %u\n", period_reg_addr, *period_reg_addr);
+    printf("PWM2 duty cycle register at %p, value: %u\n", duty_reg_addr, *duty_reg_addr);
 
+    /*
+    volatile uint32_t *pwm_registers = (volatile uint32_t *)(mapped_base + register_offset);
 
+    // -------- Configuration (Based on Usage Flow) --------
+
+    // 1. Disable PWM channel
+    pwm_registers[PWM2_CTRL_OFFSET / 4] &= ~PWM_CTRL_TIMER_EN;  
+
+    // ... (Prescale, Clock selection -  not provided in usage flow)
+
+    // 3. Output mode, Polarity (replace with your choices)
+    pwm_registers[PWM2_CTRL_OFFSET / 4] &= ~PWM_POLARITY_MASK; // Clear existing polarity
+    pwm_registers[PWM2_CTRL_OFFSET / 4] |= PWM_DUTY_POSITIVE | PWM_INACTIVE_NEGATIVE; 
+
+    // 4. For one-shot mode, configure PWM_PWMx_CTRL.rpt (omitted for continuous)
+
+    // 5. Choose continuous mode
+    pwm_registers[PWM2_CTRL_OFFSET / 4] |= PWM_CONTINUOUS;  
+
+    // 6. (Omitted - Assumed interrupts not needed) 
+    // 7. (Omitted - Assumed disabled by default)
+
+    // -------- Set Period and Duty Cycle --------
+    // You'll need to calculate register values based on your clock and desired timing
+
+    pwm_registers[PWM2_PERIOD_OFFSET / 4] = ; 
+    pwm_registers[PWM2_DUTY_OFFSET / 4] = ; 
+
+    // -------- Enable the PWM --------
+    pwm_registers[PWM2_CTRL_OFFSET / 4] |= PWM_CTRL_OUTPUT_EN | PWM_CTRL_TIMER_EN ; 
+    */
 }
 
 void pwm_start(void* pwm_regs) 
 {
     uint32_t *pwm2_regs = (uint32_t*)pwm_regs;
-    pwm2_regs[PWM2_CTRL_OFFSET / sizeof(uint32_t)] |= PWM2_EN;
+    pwm2_regs[PWM2_CTRL_OFFSET / sizeof(uint32_t)] |= PWM_ENABLE;
     printf("Enabled PWM with pwm2_regs value\n");
 
-    for (int i=0;i < (sizeof (pwm2_regs) /sizeof (pwm2_regs[0]));i++) {
+    for (int i = 0; i < (sizeof (pwm2_regs) / sizeof(pwm2_regs[0])); i++) 
+    {
         printf("%lf ", pwm2_regs[i]);
     }
 
@@ -249,7 +238,7 @@ void pwm_start(void* pwm_regs)
 void pwm_stop(void* pwm_regs) 
 {
     uint32_t *pwm2_regs = (uint32_t*)pwm_regs;
-    pwm2_regs[PWM2_CTRL_OFFSET / sizeof(uint32_t)] &= ~PWM2_EN;
+    pwm2_regs[PWM2_CTRL_OFFSET / sizeof(uint32_t)] &= ~PWM_ENABLE;
 }
 
 int pwm_enabled(void *pwm_regs) 
@@ -257,7 +246,7 @@ int pwm_enabled(void *pwm_regs)
     uint32_t *pwm2_regs = (uint32_t *)pwm_regs;
     uint32_t ctrl_reg_value = pwm2_regs[PWM2_CTRL_OFFSET / sizeof(uint32_t)];
 
-    if ((ctrl_reg_value & PWM2_EN) != 0) 
+    if ((ctrl_reg_value & PWM_ENABLE) != 0) 
     {
         return 1;
     } 
